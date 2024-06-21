30f459aae4a8d709e94ab4e1ac706d4d018c4814
M3U8 Player <120414523+anym3u8player@users.noreply.github.com>
2024-06-21 15:40:40 +0300

    Update index.tsx

--- src/pages/match/index.tsx
+++ src/pages/match/index.tsx
@@ -1,63 +1,89 @@
-import { useEffect, useMemo, useState } from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
-import { fetchMatchData, fetchMatchStats } from '../../api';
-import type { MatchData, LiveInfo, MatchStats } from '../../types';
-import dayjs from 'dayjs';
-import Tabs from '../../components/Tabs';
-import Stats from './Stats';
-import Player from '../../components/Player';
+import React, { useEffect, useMemo, useState } from 'react'
+import { useLoaderData, type LoaderFunction, useParams } from 'react-router-dom'
+import { fetchMatchData, fetchMatchStats } from '../../api'
+import type { MatchData, LiveInfo, MatchStats } from '../../types'
+import dayjs from 'dayjs'
+import Tabs from '../../components/Tabs'
+import Stats from './Stats'
+import Player from '../../components/Player'
 
-type LoaderParams = {
-  params: {
-    id: string;
-    type: string;
-  };
-};
-
-export const matchLoader = async ({ params }: LoaderParams) => {
+export const matchLoader: LoaderFunction = async ({ params }) => {
   if (params.id) {
-    const match = await fetchMatchData(params.id, params.type);
-    const stats = await fetchMatchStats(params.id);
-    return { match, stats };
+    const match = await fetchMatchData(params.id, params.type)
+    const stats = await fetchMatchStats(params.id)
+    return {
+      match,
+      stats,
+    }
   }
-  return new Response(JSON.stringify({ msg: 'data error' }), { statusText: 'No data', status: 404 });
-};
+  return Response.json(
+    { msg: 'data error' },
+    { statusText: 'No data', status: 404 }
+  )
+}
 
-const Match = () => {
-  const { id, type } = useParams<{ id: string, type: string }>();
-  const data = useLoaderData() as { match: MatchData, stats?: MatchStats };
+// const MATCH_STATUS = [1, 8, 10]
 
-  const [liveUrls] = useState<LiveInfo[]>(data.match.matchinfo.live_urls);
-  const [currentIndex, setCurrentIndex] = useState(0);
+const Match: React.FC = () => {
+  const params = useParams()
+  const data = useLoaderData() as {
+    match: MatchData
+    stats?: MatchStats
+  }
 
-  const [match, setMatch] = useState(data.match.matchinfo);
-  const [stats, setStats] = useState<MatchStats | undefined>(data.stats);
-  const [loading, setLoading] = useState(false);
-  const [updateStamp, setUpdateStamp] = useState(0);
+  const [currentLive, setCurrentLive] = useState<LiveInfo>(() => {
+    const cur = data.match.matchinfo.live_urls.find(
+      (l) => l.name === '腾讯' || l.name.includes('清')
+    )
+    return cur || data.match.matchinfo.live_urls[0]
+  })
 
-  const playing = useMemo(() => data.match.matchinfo.status === 0, [data.match.matchinfo.status]);
-  const currentLive = useMemo(() => liveUrls[currentIndex], [liveUrls, currentIndex]);
+  const [match, setMatch] = useState(data.match.matchinfo)
+  const [stats, setStats] = useState<MatchStats | undefined>(data.stats)
+  const [loading, setLoading] = useState(false)
+  const [updateStamp, setUpdateStamp] = useState(0)
+
+  const playing = useMemo(
+    () => data.match.matchinfo.status === 0,
+    [data.match.matchinfo.status]
+  )
+
+  const liveUrl = useMemo(() => currentLive.url, [currentLive.url])
 
   useEffect(() => {
-    if (playing && id && type && updateStamp) {
-      setLoading(true);
-      const res1 = fetchMatchData(id, type).then((data) => setMatch(data.matchinfo));
-      const res2 = fetchMatchStats(id).then(setStats);
-      Promise.all([res1, res2]).finally(() => setLoading(false));
+    if (playing && params.id && params.type && updateStamp) {
+      setLoading(true)
+      const res1 = fetchMatchData(params.id, params.type).then((data) =>
+        setMatch(data.matchinfo)
+      )
+      const res2 = fetchMatchStats(params.id).then(setStats)
+      Promise.all([res1, res2]).finally(() => {
+        setLoading(false)
+      })
     }
-  }, [id, type, playing, updateStamp]);
+  }, [params.id, params.type, playing, updateStamp])
+
+  // useEffect(() => {
+  //   let timer: number | undefined
+  //   if (playing) {
+  //     timer = setInterval(() => setUpdateStamp((t) => t + 1), 6000)
+  //   }
+  //   return () => clearInterval(timer)
+  // }, [playing])
 
   return (
     <section>
-      <div>{playing && <Player liveUrl={currentLive.url} />}</div>
+      <div>{playing && currentLive && <Player liveUrl={liveUrl} />}</div>
       <div className="text-center mt-4">
         {playing && (
           <div className="join">
-            {liveUrls.map((live, index) => (
+            {data.match.matchinfo.live_urls.map((live) => (
               <button
                 key={live.index}
-                onClick={() => setCurrentIndex(index)}
-                className={`btn btn-sm join-item ${index === currentIndex ? 'btn-primary' : ''}`}
+                onClick={() => setCurrentLive(live)}
+                className={btn btn-sm join-item ${
+                  live.index === currentLive.index ? 'btn-primary' : ''
+                }}
               >
                 {live.name}
               </button>
@@ -67,7 +93,11 @@ const Match = () => {
       </div>
       <div className="mx-auto my-4 p-2 max-w-lg rounded-lg lg:rounded-2xl shadow-xl text-center flex items-center justify-between gap-1 lg:gap-4">
         <div className="w-24 flex flex-col items-center">
-          <img src={match.hteam_logo} alt={match.hteam_name} className="w-10 h-10" />
+          <img
+            src={match.hteam_logo}
+            alt={match.hteam_name}
+            className="w-10 h-10"
+          />
           <div className="truncate w-full">{match.hteam_name}</div>
         </div>
         <div>
@@ -76,7 +106,11 @@ const Match = () => {
           <div>{dayjs(match.matchtime).format('YYYY-MM-DD HH:mm')}</div>
         </div>
         <div className="w-24 flex flex-col items-center">
-          <img src={match.ateam_logo} alt={match.ateam_name} className="w-10 h-10" />
+          <img
+            src={match.ateam_logo}
+            alt={match.ateam_name}
+            className="w-10 h-10"
+          />
           <div className="truncate w-full">{match.ateam_name}</div>
         </div>
       </div>
@@ -116,7 +150,5 @@ const Match = () => {
         <div>status_up_name:{match.status_up_name}</div>
       </div>
     </section>
-  );
-};
-
-export default Match;
+  )
+}
