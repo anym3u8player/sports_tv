38c9536e7339f4861d289bd17ca2b4588384c08f
M3U8 Player <120414523+anym3u8player@users.noreply.github.com>
2024-06-21 17:28:30 +0300

    Update Header.tsx

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -6,8 +6,8 @@ const Header: React.FC = () => {
   return (
     <header className="flex items-center gap-1 flex-wrap min-h-[40px]">
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/hot/1">football</NavLink>
-      <NavLink to="/hot/2">basketball</NavLink>
+      <NavLink to="/hot/1">Football</NavLink>
+      <NavLink to="/hot/2">Basketball</NavLink>
       <NavLink to="/cate">Classification</NavLink>
       <ThemeButton />
     </header>
